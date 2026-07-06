e84ec96ed4986048de799df5783b28c4b026bf3f
shou130521 <shou130521@gmail.com>
2026-07-06 21:20:43 +0900

    観戦記録投稿処理を追加

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -6,4 +6,20 @@ class RecordsController < ApplicationController
   def new
     @record = Record.new
   end
+
+  def create
+    @record = current_user.records.build(record_params)
+
+    if @record.save
+      redirect_to records_path, notice: "観戦記録を投稿しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:title, :body, :watched_on)
+  end
 end
